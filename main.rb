ab097a285e5b4a4f7f2ee1c553d9f75ead611cd4
jake <jake.wilkins@adfitech.com>
2011-11-04 17:40:30 -0500

    make it work kind of

--- main.rb
+++ main.rb
@@ -1,24 +1,42 @@
 require 'sinatra'
 require 'redis'
+require 'uri'
+require 'haml'
 #require 'mongoid'
 
 
 dir = File.expand_path(File.dirname(__FILE__))
-set :root, File.join(dir, 'app')
 set :public_folder, File.join(dir, 'public')
 
 configure do
-  $redis = 
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 end
 
 get '/' do
   redirect 'http://jakeplusbecca.us'
 end
 
-get '/:id' do
+get '/add' do
+  haml :add
+end
 
+get '/:id' do
+  redirect $redis.get(params[:id])
 end
 
 post '/add' do
+  id = gen_key
+  p params, params["shortener"]["url"]
+  $redis.set(id, params["shortener"]['url'])
+  "http://j-b.us/#{id}"
+end
 
+def gen_key
+  begin
+    o =  [('a'..'z'),('A'..'Z')].map{|i| i.to_a}.flatten;
+    string  =  (0..4).map{ o[rand(o.length)]  }.join;
+    puts "testing #{string}"
+  end while !$redis.get(string).nil?
+  string
 end
