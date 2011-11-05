cfd9208a03d6bbf5facccbedcb87b6278476324f
jake <jake.wilkins@adfitech.com>
2011-11-05 16:00:51 -0500

    revert to more simple data store
    just cause it's an option.

--- main.rb
+++ main.rb
@@ -2,7 +2,6 @@ require 'sinatra'
 require 'redis'
 require 'uri'
 require 'haml'
-require 'digest/sha1'
 
 
 dir = File.expand_path(File.dirname(__FILE__))
@@ -11,30 +10,19 @@ set :public_folder, File.join(dir, 'public')
 configure do
   uri = URI.parse(ENV["REDISTOGO_URL"])
   $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  $url_base = ENV['URL_BASE']
 end
 
 get '/' do
-  redirect ENV['DEFAULT_URL'] || '/index'
+  redirect ENV['DEFAULT_URL'] || '/add'
 end
 
 get '/add' do
   haml :add
 end
 
-get '/index' do
-  @shortens = Array.new
-  $redis.keys('*:data').each do |key|
-    p "  fetching data on #{key}"
-    @shortens << $redis.hgetall(key)
-  end
-  p @shortens
-  haml :index
-end
-
 get '/:id' do
-  sha = $redis.get(params[:id])
-  url = $redis.hget("#{sha}:data", 'url')
-  $redis.hincrby("#{sha}:data", 'click-count', 1)
+  url = $redis.get(params[:id])
   url.insert(0, 'http://') unless url[0..6] == 'http://'
   redirect url
 end
@@ -44,9 +32,8 @@ post '/add' do
 end
 
 def fetch_shortened_url(url)
-  id = check_cache(url)
-  id ||= shorten(url)
-  "http://j-b.us/#{id}"
+  id = shorten(url)
+  "http://#{$url_base}/#{id}"
 end
 
 def shorten(url)
@@ -55,19 +42,7 @@ def shorten(url)
     key  =  (0..4).map{ o[rand(o.length)]  }.join;
     puts "testing #{key}"
   end while !$redis.get(key).nil?
-  sha = Digest::SHA1.hexdigest(url)
-  $redis.set(key, sha)
-  $redis.hmset("#{sha}:data", 'shortened', key, 'url', url, 'set-count', 1)
+  $redis.set(key, url)
   key
 end
 
-def check_cache(url)
-  sha = Digest::SHA1.hexdigest(url)
-  short = $redis.hgetall("#{sha}:data")
-  unless short == {}
-    $redis.hincrby("#{sha}:data", 'set-count', 1) 
-    return short['shortened']
-  else
-    return nil
-  end
-end
